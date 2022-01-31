8bbe1e0b85d9cf14780f13f1a14b905f22e122e4
EC2 Default User <ec2-user@ip-172-31-49-0.ec2.internal>
2022-01-31 19:48:37 +0000

    lab2

---
+++ lec2.py
@@ -0,0 +1,22 @@
+"""
+lec 2
+"""
+
+print("hello world")
+
+
+print(123)
+print(123.)
+print("123")
+print('hello world')
+print("Hello World".upper() )
+print ("Hello World".lower () )
+print("Hello World".strip())
+print("Hello World".split('o'))
+print("Hello" + "World")
+print(1+2)
+print(2-6)
+print(2/6)
+print(2*3)
+my_int=123
+print(my_int)
